0e7ca2add47b15a35662d4ae5b70a68f8100596c
RuijieKang <ruijie.kang@gmail.com>
2021-09-14 10:52:14 -0400

    Make reactive stream test more comparable

--- poc-kafka-stream/src/test/java/ca/donegall/pockafka/ReactiveStreamTest.java
+++ poc-kafka-stream/src/test/java/ca/donegall/pockafka/ReactiveStreamTest.java
@@ -3,14 +3,18 @@ package ca.donegall.pockafka;
 import java.time.Duration;
 import org.junit.jupiter.api.Test;
 import reactor.core.publisher.Flux;
+import reactor.core.scheduler.Schedulers;
 import reactor.test.StepVerifier;
 
-public class ReactiveTest {
+public class ReactiveStreamTest {
     @Test
     public void testReactiveStream(){
         Flux<Long> producer = Flux.interval(Duration.ofSeconds(1)).take(4).log();
         
-        Flux<Long> processed = producer.map(number -> number * number).log();
+        Flux<Long> processed = producer.publishOn(Schedulers.parallel())
+            .map(number -> number * number)
+            .doOnNext(number ->{System.out.println("Current thread for  " + number + " " + Thread.currentThread().getName());})
+            .log();
 
         StepVerifier.create(processed)
             .expectNext(0L)
